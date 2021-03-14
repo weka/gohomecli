7cb540506ee2ef51b70214793cf85e945f528b81
Assaf Giladi <assaf@weka.io>
2021-03-14 11:18:57 +0200

    feat: upload binaries to github, when pusing to "ga" branch

--- deploy.sh
+++ deploy.sh
@@ -23,3 +23,35 @@ else
 fi
 
 aws s3 cp "bin" "s3://gohomecli/$folder/"  --region "eu-central-1" --recursive
+
+if [[ "$BUILDKITE_BRANCH" == "ga" ]]; then
+  if [ -z "$DEPLOY_APP_ID" ]; then
+    echo "You must supply DEPLOY_APP_ID environment variable !"
+    exit 1
+  fi
+
+  if [ -z "$DEPLOY_APP_PRIVATE_KEY" ]; then
+    echo "You must supply DEPLOY_APP_PRIVATE_KEY environment variable !"
+    exit 1
+  fi
+  docker build -t github-token . -f python.Dockerfile
+  eval "$(docker run -e "DEPLOY_APP_ID=$DEPLOY_APP_ID" -e "DEPLOY_APP_PRIVATE_KEY=$DEPLOY_APP_PRIVATE_KEY" github-token)"
+
+  AUTH="Authorization: token $GITHUB_TOKEN"
+  result=$(curl \
+    -X POST \
+    -H "$AUTH" \
+    https://api.github.com/repos/weka/gohomecli/releases \
+    -d "{\"tag_name\":\"$BUILD_VERSION\", \"body\":\"GA release\"}")
+
+  id=$(echo "$result" | jq -c ".id")
+
+  filenames="bin/homecli_linux_amd64 bin/homecli_darwin_amd64"
+  for filename in $filenames; do
+    curl \
+      -H "$AUTH" \
+      -H "Content-Type: $(file -b --mime-type "$filename")" \
+      --data-binary @"$filename" \
+      "https://uploads.github.com/repos/weka/gohomecli/releases/$id/assets?name=$(basename "$filename")"
+  done
+fi
