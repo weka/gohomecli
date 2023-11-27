bec6e63b798405c8ea1b1a3d9146ced67815874e
Maxym Fugol <110974066+mfuhol-weka@users.noreply.github.com>
2023-11-27 12:46:52 +0100

    feat: wekahome bundle internal library (#10)
    * add bundle lib
    
    * apply go.mod tidy
    
    * fixes
    
    * add files digest to manifest
    
    * rename manifest file name
    
    * apply go.mod tidy
    
    * apply gofumpt

---
+++ internal/bundle/manifest.go
@@ -0,0 +1,33 @@
+package bundle
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+const (
+	versionFileName = "MANIFEST.json"
+)
+
+type Manifest struct {
+	WekaHome     string            `json:"wekaHome"`
+	K3S          string            `json:"k3S"`
+	DockerImages map[string]string `json:"dockerImages"`
+	FilesDigest  map[string]string `json:"filesDigest"`
+}
+
+func GetManifest() (Manifest, error) {
+	var manifest Manifest
+
+	versionsBytes, err := ReadFileBytes(versionFileName)
+	if err != nil {
+		return Manifest{}, fmt.Errorf("failed to read bundle versions: %w", err)
+	}
+
+	err = json.Unmarshal(versionsBytes, &manifest)
+	if err != nil {
+		return Manifest{}, fmt.Errorf("failed to unmarshal bundle versions: %w", err)
+	}
+
+	return manifest, nil
+}
