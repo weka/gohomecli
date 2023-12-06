a1fc18a0dd0c08546294070b501b670c1f2b909b
Maxym Fugol <110974066+mfuhol-weka@users.noreply.github.com>
2023-12-06 16:02:41 +0100

    feat: images import (#20)
    * import images skeleton
    
    * first working iteration
    
    * implement import images endpoint

--- internal/cli/k3s/k3s.go
+++ internal/cli/k3s/k3s.go
@@ -1,6 +1,8 @@
 package k3s
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 	"github.com/weka/gohomecli/internal/bundle"
 	"github.com/weka/gohomecli/internal/cli/app/hooks"
@@ -30,18 +32,31 @@ func init() {
 
 		k3sUpgradeCmd.Flags().StringVar(&k3sUpgradeConfig.BundlePath, "bundle", bundle.BundlePath(), "bundle with k3s to install")
 		k3sUpgradeCmd.Flags().BoolVar(&k3sUpgradeConfig.Debug, "debug", false, "enable debug mode")
-
 		k3sUpgradeCmd.Flags().MarkHidden("bundle")
 		k3sUpgradeCmd.Flags().MarkHidden("debug")
 
+		k3sImageImportCmd.Flags().StringVar(&k3sImportConfig.BundlePath, "bundle", bundle.BundlePath(), "bundle with images to load")
+		k3sImageImportCmd.Flags().BoolVar(&k3sImportConfig.FailFast, "fail-fast", false, "fail on first error")
+		k3sImageImportCmd.Flags().BoolVar(&k3sImportConfig.FromBundle, "from-bundle", false, "import images from bundle")
+		k3sImageImportCmd.Flags().StringSliceVarP(&k3sImportConfig.ImagePaths, "image-path", "f", nil, "images to import")
+		k3sImageImportCmd.Flags().MarkHidden("bundle")
+		k3sImageImportCmd.MarkFlagsMutuallyExclusive("from-bundle", "image-path")
+
 		k3sCmd.AddCommand(k3sInstallCmd)
 		k3sCmd.AddCommand(k3sUpgradeCmd)
+		k3sCmd.AddCommand(k3sImageImportCmd)
 	})
 }
 
 var (
 	k3sInstallConfig k3s.InstallConfig
 	k3sUpgradeConfig k3s.UpgradeConfig
+	k3sImportConfig  struct {
+		BundlePath string
+		FromBundle bool
+		ImagePaths []string
+		FailFast   bool
+	}
 )
 
 var K3SGroup = &cobra.Group{
@@ -73,5 +88,21 @@ var k3sUpgradeCmd = &cobra.Command{
 	Short: "upgrade cluster to bundled version",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		return k3s.Upgrade(cmd.Context(), k3sUpgradeConfig)
+	},
+}
+
+var k3sImageImportCmd = &cobra.Command{
+	Use:   "import-images",
+	Short: "import images from bundle",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if !k3sImportConfig.FromBundle && len(k3sImportConfig.ImagePaths) == 0 {
+			return fmt.Errorf("%w: either --from-bundle or --image-path must be specified", utils.ErrValidationFailed)
+		}
+
+		if k3sImportConfig.FromBundle {
+			return k3s.ImportBundleImages(cmd.Context(), k3sImportConfig.BundlePath, k3sImportConfig.FailFast)
+		}
+
+		return k3s.ImportImages(cmd.Context(), k3sImportConfig.ImagePaths, k3sImportConfig.FailFast)
 	},
 }
---
+++ internal/k3s/images.go
@@ -0,0 +1,193 @@
+package k3s
+
+import (
+	"bytes"
+	"compress/gzip"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"os/exec"
+	"runtime"
+
+	"github.com/rs/zerolog"
+	"github.com/weka/gohomecli/internal/bundle"
+	"github.com/weka/gohomecli/internal/utils"
+)
+
+func getCurrentPlatform() string {
+	return fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH)
+}
+
+func unzippedData(imagePath string) ([]byte, error) {
+	file, err := os.Open(imagePath)
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	buffer := make([]byte, 512)
+	_, err = file.Read(buffer)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = file.Seek(0, 0)
+	if err != nil {
+		return nil, err
+	}
+
+	mime := http.DetectContentType(buffer)
+	if mime == "application/x-gzip" {
+		reader, err := gzip.NewReader(file)
+		if err != nil {
+			return nil, err
+		}
+
+		return io.ReadAll(reader)
+	}
+
+	return io.ReadAll(file)
+}
+
+func plainLogWriter(level zerolog.Level) io.WriteCloser {
+	return utils.NewWriteScanner(func(b []byte) {
+		logger.WithLevel(level).Msg(string(b))
+	})
+}
+
+func ImportImages(ctx context.Context, imagePaths []string, failFast bool) error {
+	var importErrors []error
+	for _, imagePath := range imagePaths {
+		logger := logger.With().Str("imagePath", imagePath).Logger()
+		if ctx.Err() != nil {
+			if errors.Is(ctx.Err(), context.Canceled) {
+				logger.Info().Msg("Context canceled")
+				return ctx.Err()
+			} else if errors.Is(ctx.Err(), context.DeadlineExceeded) {
+				logger.Info().Msg("Context deadline exceeded")
+				return ctx.Err()
+			} else {
+				logger.Warn().Msg("Context error")
+				return ctx.Err()
+			}
+		}
+
+		data, err := unzippedData(imagePath)
+		if err != nil {
+			logger.Warn().
+				Err(err).
+				Msg("Failed to read image data")
+
+			if failFast {
+				return err
+			} else {
+				importErrors = append(importErrors, err)
+				continue
+			}
+		}
+
+		reader := bytes.NewBuffer(data)
+		cmd := exec.Command(
+			"k3s", "ctr", "image", "import",
+			"--platform", getCurrentPlatform(),
+			"--", "-",
+		)
+		cmd.Stdin = reader
+
+		stderr, err := cmd.StderrPipe()
+		if err != nil {
+			logger.Warn().
+				Err(err).
+				Msg("Failed to capture import command output")
+
+			if failFast {
+				return err
+			} else {
+				importErrors = append(importErrors, err)
+				continue
+			}
+		}
+
+		stdout, err := cmd.StdoutPipe()
+		if err != nil {
+			logger.Warn().
+				Err(err).
+				Msg("Failed to capture import command output")
+
+			if failFast {
+				return err
+			} else {
+				importErrors = append(importErrors, err)
+				continue
+			}
+		}
+
+		logger.Debug().Strs("command", cmd.Args).Msg("Running import command")
+		err = cmd.Start()
+		if err != nil {
+			logger.Warn().
+				Err(err).
+				Msg("Failed run import command")
+
+			if failFast {
+				return err
+			} else {
+				importErrors = append(importErrors, err)
+				continue
+			}
+		}
+
+		go io.Copy(plainLogWriter(zerolog.InfoLevel), stdout)
+		go io.Copy(plainLogWriter(zerolog.ErrorLevel), stderr)
+
+		err = cmd.Wait()
+		if err != nil {
+			logger.Warn().
+				Err(err).
+				Msg("Failed to import image")
+
+			if failFast {
+				return err
+			} else {
+				importErrors = append(importErrors, err)
+				continue
+			}
+		}
+	}
+
+	if len(importErrors) > 0 {
+		return errors.Join(importErrors...)
+	}
+
+	return nil
+}
+
+func ImportBundleImages(ctx context.Context, bundlePathOverride string, failFast bool) error {
+	if bundlePathOverride != "" {
+		if err := bundle.SetBundlePath(bundlePathOverride); err != nil {
+			return err
+		}
+	}
+
+	imagePaths := []string{}
+	err := bundle.Walk("images", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() {
+			imagePaths = append(imagePaths, path)
+		}
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	return ImportImages(ctx, imagePaths, failFast)
+}
