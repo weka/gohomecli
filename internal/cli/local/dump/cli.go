188597d64d6a3c5c5bbb97b7f2b6cd1506244e58
Bohdan Dymchenko <bohdan.dymchenko@weka.io>
2023-12-18 20:56:22 +0200

    fixup

--- internal/cli/local/dump/cli.go
+++ internal/cli/local/dump/cli.go
@@ -1,16 +1,10 @@
 package dump
 
 import (
-	"bytes"
-	"fmt"
-	"io"
-	"os/exec"
-
 	"github.com/spf13/cobra"
 
 	"github.com/weka/gohomecli/internal/cli/app/hooks"
 	"github.com/weka/gohomecli/internal/local/dump"
-	"github.com/weka/gohomecli/internal/utils"
 )
 
 var dumpCmd = &cobra.Command{
@@ -18,16 +12,16 @@ var dumpCmd = &cobra.Command{
 	Short:     "Dump cluster information for debugging and save it into OUTPUT_ARCHIVE",
 	Args:      cobra.ExactArgs(1),
 	ValidArgs: []string{"OUTPUT_ARCHIVE"},
-	RunE:      runDumpScript,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		config.Output = args[0]
+		return dump.Dump(cmd.Context(), config)
+	},
 }
 
-var Cli hooks.Cli
-
-var config struct {
-	InclideSensitive bool
-	FullDiskScan     bool
-	Verbose          bool
-}
+var (
+	Cli    hooks.Cli
+	config dump.Config
+)
 
 func init() {
 	Cli.AddHook(func(appCmd *cobra.Command) {
@@ -37,42 +31,4 @@ func init() {
 		dumpCmd.Flags().BoolVar(&config.InclideSensitive, "include-sensitive", false, "Include sensitive data in the archive (e.g., values overrides). Use with caution.")
 		dumpCmd.Flags().BoolVar(&config.FullDiskScan, "full-disk-scan", false, "Perform a full disk scan and include the detailed disk usage information in the archive.")
 	})
-}
-
-func runDumpScript(cmd *cobra.Command, args []string) error {
-	opts := []string{"-s", "-", args[0]}
-
-	if config.InclideSensitive {
-		opts = append(opts, "--include-sensitive")
-	}
-	if config.FullDiskScan {
-		opts = append(opts, "--full-disk-scan")
-	}
-	if config.Verbose {
-		opts = append(opts, "-v")
-	}
-
-	shell := exec.CommandContext(cmd.Context(), "bash", opts...)
-	shell.Stdin = bytes.NewReader(dump.DumpScript)
-
-	stdout, err := shell.StdoutPipe()
-	if err != nil {
-		return err
-	}
-	stderr, err := shell.StderrPipe()
-	if err != nil {
-		return err
-	}
-
-	infoLogWriter := utils.NewWriteScanner(func(b []byte) {
-		fmt.Println(string(b))
-	})
-	errorLogWriter := utils.NewWriteScanner(func(b []byte) {
-		fmt.Println(string(b))
-	})
-
-	go io.Copy(infoLogWriter, stdout)
-	go io.Copy(errorLogWriter, stderr)
-
-	return shell.Run()
 }
--- internal/local/dump/dump.go
+++ internal/local/dump/dump.go
@@ -1,6 +1,65 @@
 package dump
 
-import _ "embed"
+import (
+	"bytes"
+	"context"
+	_ "embed"
+	"fmt"
+	"io"
+	"os/exec"
+
+	"github.com/weka/gohomecli/internal/utils"
+)
 
 //go:embed dump.sh
-var DumpScript []byte
+var script []byte
+
+type Config struct {
+	Output           string
+	InclideSensitive bool
+	FullDiskScan     bool
+	Verbose          bool
+}
+
+func (c Config) toArgs() []string {
+	opts := []string{c.Output}
+
+	if c.InclideSensitive {
+		opts = append(opts, "--include-sensitive")
+	}
+	if c.FullDiskScan {
+		opts = append(opts, "--full-disk-scan")
+	}
+	if c.Verbose {
+		opts = append(opts, "-v")
+	}
+	return opts
+}
+
+func Dump(ctx context.Context, config Config) error {
+	bashArgs := []string{"-s", "-"}
+
+	shell := exec.CommandContext(ctx, "bash", append(bashArgs, config.toArgs()...)...)
+	shell.Stdin = bytes.NewReader(script)
+
+	stdout, err := shell.StdoutPipe()
+	if err != nil {
+		return err
+	}
+	stderr, err := shell.StderrPipe()
+	if err != nil {
+		return err
+	}
+
+	infoLogWriter := utils.NewWriteScanner(func(b []byte) {
+		fmt.Println(string(b))
+	})
+	errorLogWriter := utils.NewWriteScanner(func(b []byte) {
+		fmt.Println(string(b))
+	})
+
+	go io.Copy(infoLogWriter, stdout)
+	go io.Copy(errorLogWriter, stderr)
+
+	return shell.Run()
+}
