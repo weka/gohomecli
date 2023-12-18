6c71c4b48415615986a82897b561ef42572ecffd
Maxym Fugol <110974066+mfuhol-weka@users.noreply.github.com>
2023-12-18 13:34:41 +0100

    feat: additional user configuration for chart install (#25)
    * add extra configuration
    
    * add forwarding configuration
    
    * add cli group for install command

--- internal/cli/install/install.go
+++ internal/cli/install/install.go
@@ -13,10 +13,16 @@ import (
 
 var Cli hooks.Cli
 
+var installGroup = &cobra.Group{
+	ID:    "install",
+	Title: "Manage installations",
+}
+
 var installCmd = &cobra.Command{
-	Use:   "install",
-	Short: "Manage installations",
-	Long:  "Manage wekahome installations",
+	Use:     "install",
+	Short:   "Manage installations",
+	Long:    "Manage wekahome installations",
+	GroupID: "install",
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
 		if !env.VerboseLogging {
 			utils.SetGlobalLoggingLevel(utils.InfoLevel)
@@ -26,6 +32,7 @@ var installCmd = &cobra.Command{
 
 func init() {
 	Cli.AddHook(func(appCmd *cobra.Command) {
+		appCmd.AddGroup(installGroup)
 		appCmd.AddCommand(installCmd)
 		k3s.Cli.InitCobra(installCmd)
 		chart.Cli.InitCobra(installCmd)
--- internal/install/chart/configuration.go
+++ internal/install/chart/configuration.go
@@ -1,6 +1,32 @@
 package chart
 
-// Configuration flat options for the chart
+// Configuration flat options for the chart, pointers are used to distinguish between empty and unset values
 type Configuration struct {
-	Host string `json:"host"`
+	Host    *string `json:"host"`    // ingress host
+	TLS     *bool   `json:"tls"`     // ingress tls enabled
+	TLSCert *string `json:"tlsCert"` // ingress tls cert
+	TLSKey  *string `json:"tlsKey"`  // ingress tls key
+
+	SMTPHost        *string `json:"smtpHost"`        // smtp server host
+	SMTPPort        *int    `json:"smtpPort"`        // smtp server port
+	SMTPUser        *string `json:"smtpUser"`        // smtp server user
+	SMTPPassword    *string `json:"smtpPassword"`    // smtp server password
+	SMTPInsecure    *bool   `json:"smtpInsecure"`    // smtp insecure connection
+	SMTPSender      *string `json:"smtpSender"`      // smtp sender name
+	SMTPSenderEmail *string `json:"smtpSenderEmail"` // smtp sender email
+
+	DiagnosticsRetentionDays *int `json:"diagnosticsRetentionDays"` // diagnostics retention days
+	EventsRetentionDays      *int `json:"eventsRetentionDays"`      // events retention days
+
+	ForwardingEnabled                   bool    `json:"forwardingEnabled"`                   // forwarding enabled
+	ForwardingUrl                       *string `json:"forwardingUrl"`                       // forwarding url override
+	ForwardingEnableEvents              *bool   `json:"forwardingEnableEvents"`              // forwarding enable events
+	ForwardingEnableUsageReports        *bool   `json:"forwardingEnableUsageReports"`        // forwarding enable usage reports
+	ForwardingEnableAnalytics           *bool   `json:"forwardingEnableAnalytics"`           // forwarding enable analytics
+	ForwardingEnableDiagnostics         *bool   `json:"forwardingEnableDiagnostics"`         // forwarding enable diagnostics
+	ForwardingEnableStats               *bool   `json:"forwardingEnableStats"`               // forwarding enable stats
+	ForwardingEnableClusterRegistration *bool   `json:"forwardingEnableClusterRegistration"` // forwarding enable cluster registration
+
+	Autoscaling     bool `json:"autoscaling"`        // enable services autoscaling
+	WekaNodesServed *int `json:"wekaNodesMonitored"` // number of weka nodes to monitor, controls load preset
 }
