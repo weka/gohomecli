8c94ba668c40f54978e75a09b66ac14f8f4e6035
Bohdan Dymchenko <bohdan.dymchenko@weka.io>
2024-01-03 11:44:27 +0200

    fix: add omitempty for config

--- internal/local/config/v1/v1.go
+++ internal/local/config/v1/v1.go
@@ -2,42 +2,42 @@ package config_v1
 
 // Configuration flat options for the chart, pointers are used to distinguish between empty and unset values
 type Configuration struct {
-	Host        string   `json:"host"`         // ingress host
-	NodeIP      string   `json:"node_ip"`      // node ip to bind on as primary internal ip
-	ExternalIPs []string `json:"external_ips"` // list of external ip addresses, optional
+	Host        string   `json:"host,omitempty"`         // ingress host
+	NodeIP      string   `json:"node_ip,omitempty"`      // node ip to bind on as primary internal ip
+	ExternalIPs []string `json:"external_ips,omitempty"` // list of external ip addresses, optional
 
 	TLS struct {
-		Enabled bool   `json:"enabled"` // ingress tls enabled
-		Cert    string `json:"cert"`    // ingress tls cert
-		Key     string `json:"key"`     // ingress tls key
+		Enabled bool   `json:"enabled"`        // ingress tls enabled
+		Cert    string `json:"cert,omitempty"` // ingress tls cert
+		Key     string `json:"key,omitempty"`  // ingress tls key
 	} `json:"tls"`
 
 	SMTP struct {
-		Host        string `json:"host"`        // smtp server host
-		Port        int    `json:"port"`        // smtp server port
-		User        string `json:"user"`        // smtp server user
-		Password    string `json:"password"`    // smtp server password
-		Insecure    bool   `json:"insecure"`    // smtp insecure connection
-		Sender      string `json:"sender"`      // smtp sender name
-		SenderEmail string `json:"senderEmail"` // smtp sender email
+		Host        string `json:"host,omitempty"`        // smtp server host
+		Port        int    `json:"port,omitempty"`        // smtp server port
+		User        string `json:"user,omitempty"`        // smtp server user
+		Password    string `json:"password,omitempty"`    // smtp server password
+		Insecure    bool   `json:"insecure,omitempty"`    // smtp insecure connection
+		Sender      string `json:"sender,omitempty"`      // smtp sender name
+		SenderEmail string `json:"senderEmail,omitempty"` // smtp sender email
 	} `json:"smtp"`
 
 	RetentionDays struct {
-		Diagnostics int `json:"diagnostics"` // diagnostics retention days
-		Events      int `json:"events"`      // events retention days
+		Diagnostics int `json:"diagnostics,omitempty"` // diagnostics retention days
+		Events      int `json:"events,omitempty"`      // events retention days
 	} `json:"retentionDays"`
 
 	Forwarding struct {
-		Enabled                   bool   `json:"enabled"`                   // forwarding enabled
-		Url                       string `json:"url"`                       // forwarding url override
-		EnableEvents              bool   `json:"enableEvents"`              // forwarding enable events
-		EnableUsageReports        bool   `json:"enableUsageReports"`        // forwarding enable usage reports
-		EnableAnalytics           bool   `json:"enableAnalytics"`           // forwarding enable analytics
-		EnableDiagnostics         bool   `json:"enableDiagnostics"`         // forwarding enable diagnostics
-		EnableStats               bool   `json:"enableStats"`               // forwarding enable stats
-		EnableClusterRegistration bool   `json:"enableClusterRegistration"` // forwarding enable cluster registration
+		Enabled                   bool   `json:"enabled"`                             // forwarding enabled
+		Url                       string `json:"url,omitempty"`                       // forwarding url override
+		EnableEvents              bool   `json:"enableEvents,omitempty"`              // forwarding enable events
+		EnableUsageReports        bool   `json:"enableUsageReports,omitempty"`        // forwarding enable usage reports
+		EnableAnalytics           bool   `json:"enableAnalytics,omitempty"`           // forwarding enable analytics
+		EnableDiagnostics         bool   `json:"enableDiagnostics,omitempty"`         // forwarding enable diagnostics
+		EnableStats               bool   `json:"enableStats,omitempty"`               // forwarding enable stats
+		EnableClusterRegistration bool   `json:"enableClusterRegistration,omitempty"` // forwarding enable cluster registration
 	} `json:"forwarding"`
 
-	Autoscaling     bool `json:"autoscaling"`        // enable services autoscaling
-	WekaNodesServed int  `json:"wekaNodesMonitored"` // number of weka nodes to monitor, controls load preset
+	Autoscaling     bool `json:"autoscaling"`                  // enable services autoscaling
+	WekaNodesServed int  `json:"wekaNodesMonitored,omitempty"` // number of weka nodes to monitor, controls load preset
 }
