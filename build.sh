4b705c5578bca41bf72601a50e469b760a78da0f
Bohdan Dymchenko <bohdan.dymchenko@weka.io>
2024-01-25 13:09:14 +0200

    chore: add ldflags to reduce binary size

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@
 BUILD_VERSION=$1
 BUILD_TIME=$(date +'%Y-%m-%d_%T')
 LD_FLAGS="-X main.BuildVersion=$BUILD_VERSION -X main.BuildTime=$BUILD_TIME"
+LD_FLAGS="$LD_FLAGS -s -w"
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="$LD_FLAGS" -o bin/homecli_linux_amd64 cmd/homecli/*.go
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags="$LD_FLAGS" -o bin/homecli_darwin_amd64 cmd/homecli/*.go
