22fef1514101890ac3232597b0b27d5383ac9d3e
Bohdan Dymchenko <142878290+bohdand-weka@users.noreply.github.com>
2024-01-25 16:40:31 +0200

    chore: add upx compression to build.sh (#53)

--- build.sh
+++ build.sh
@@ -6,3 +6,15 @@ LD_FLAGS="-X main.BuildVersion=$BUILD_VERSION -X main.BuildTime=$BUILD_TIME"
 LD_FLAGS="$LD_FLAGS -s -w"
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="$LD_FLAGS" -o bin/homecli_linux_amd64 cmd/homecli/*.go
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags="$LD_FLAGS" -o bin/homecli_darwin_amd64 cmd/homecli/*.go
+
+# Add UPX compression
+
+UPX_VERSION="${UPX_VERSION:-4.2.2}"
+UPX_PLATFORM="${UPX_PLATFORM:-amd64_linux}"
+
+if [ ! -d "upx-${UPX_VERSION}-${UPX_PLATFORM}" ]; then
+  wget "https://github.com/upx/upx/releases/download/v${UPX_VERSION}/upx-${UPX_VERSION}-${UPX_PLATFORM}.tar.xz"
+  tar xf "upx-${UPX_VERSION}-${UPX_PLATFORM}.tar.xz" "upx-${UPX_VERSION}-${UPX_PLATFORM}/upx"
+fi
+
+"upx-${UPX_VERSION}-${UPX_PLATFORM}/upx" bin/*
